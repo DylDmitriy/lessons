07f2bbb1a6004aa3b4fd3d730dedc56c45910435
Dmytro Dyl <dyl.dmitriy@gmail.com>
2022-03-18 11:17:22 +0200

    new file lesson3.py

--- lesson2.py
+++ lesson2.py
@@ -1,2 +1,1 @@
 print(__builtins__)
-
--- lesson3.py
+++ lesson3.py
@@ -2,4 +2,10 @@ a = "This is my {} book".format("new")
 print(a)
 a = "This is my {} book".format("замена в строке")
 print(a)
+a = "This is my замена в строке book"
+if a.count("строке"):
+    print("Да")
+else:
+    print("Нет")
+
 
