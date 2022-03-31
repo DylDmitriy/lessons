9d1ac2526c522a5a2df975729243499ad260cd5e
Dmytro Dyl <dyl.dmitriy@gmail.com>
2022-03-31 12:24:41 +0300

    new file lesson8.py

--- lesson8.py
+++ lesson8.py
@@ -3,7 +3,7 @@ result = {}
 for key in text.split():
     if not result.get(key) == None:
         result[key] += 1
-      else:
+    else:
         result[key] = 1
 for key, value in result.items():
     print(f'Слово: {key}, Количество: {value}')
