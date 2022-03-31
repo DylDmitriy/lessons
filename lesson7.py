8ef8eb3fb0d328b4537f7341533d82095bc40c45
Dmytro Dyl <dyl.dmitriy@gmail.com>
2022-03-31 12:03:03 +0300

    new file lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,1 +1,8 @@
-print(len(set(set(input().split())^set(input().split()))))
+set_1 = {1, 2, 3, 4}
+set_2 = {2, 3, 4, 5}
+if set_1 >= set_2:
+    print (True)
+else:
+    print (False)
+#set_3 = set_1.issuperset(set_2) вариант
+#print(set_3)
